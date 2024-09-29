3589ec693ca19e8bbadf2617962b24db79fe1b0e
Borys855 <chirboris123@gmail.com>
2024-09-29 13:47:24 +0300

    Homework_student commit

---
+++ classwork.py
@@ -0,0 +1,42 @@
+'''
+class Student:
+    print("hi i am Vasya")
+    def _init_(self):
+        self.height=20
+        print("human")
+        First_student = Student()
+'''
+'''
+class Student:
+    amout_of_students =0
+    def __init__(self, height=160):
+        self.height = height
+        student.amount_of_students += 1
+        nick = student()
+        kate = student(height=170)
+        print("nick.amount_of_students")
+        print("student.about_of_students")
+    '''
+class Student:
+    def _init_(self):
+        print(self.height)
+        self.height += 10
+Vasya = Student()
+Student = Student()
+class money:
+    print("10 dollars")
+    def _init_(self):
+      self.money=10
+      print("1 coint")
+      First_money = money()
+
+class money:
+    print("+10 dollars")
+    def _init_(self):
+        self.money += 10
+        dollars = 10()
+
+    print("-10 dollars")
+    def _init_(self):
+        self.money -= 10
+        dollars = 10()
